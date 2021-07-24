de72bfb774046e1757c6252d6c65c8e7ba32c974
Рашид <ri-post@yandex.ru>
2021-07-24 17:28:28 +0500

    GET-запросы от авторизованного пользователя работают

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+# from rest_framework.relations import SlugRelatedField  # зачем такой импорт???????????????
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    # author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username', read_only=True
+    )
 
     class Meta:
         fields = '__all__'
@@ -21,3 +24,23 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+    following = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'  # !read_only!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    )
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Group
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,28 @@
-# TODO:  Напишите свой вариант
+# * При описании вьюсетов для некоторых моделей имеет смысл наследоваться от
+# собственного базового вьюсета.
+# * При попытке подписаться на самого себя, пользователь должен получить
+# информативное сообщение об ошибке.
+from rest_framework import viewsets
+
+from posts.models import Comment, Follow, Group, Post
+from .serializers import CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
